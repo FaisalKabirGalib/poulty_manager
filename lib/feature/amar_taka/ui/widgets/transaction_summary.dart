ccbbe5568554e922dcb48f5907ebc7cccd3a1025
Navid Rahman <navidrahman92@gmail.com>
2023-12-18 12:32:16 +0600

    Organised

---
+++ lib/feature/amar_taka/ui/widgets/transaction_summary.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:poulty_manager/config/config.dart';
+import 'package:poulty_manager/config/constant/constant.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+Widget transactionSummary(
+    {name, paymentType, transactionAmount, transactionDate}) {
+  return Container(
+    child: [
+      [
+        Styled.text('শেখ মুজিবুর রহমান')
+            .fontSize(16)
+            .fontWeight(FontWeight.bold),
+        Styled.text('পাওনা  (বিকাশ)').textColor(Colors.grey).fontSize(14),
+      ].toColumn(
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      [
+        Styled.text('+ ৮০০০.০০')
+            .textColor(AppColors.successColor)
+            .fontSize(16)
+            .fontWeight(FontWeight.bold),
+        Styled.text('১২ নভেম্বার, ২:৪৫ PM')
+            .textColor(Colors.black)
+            .fontSize(14),
+      ].toColumn(
+        crossAxisAlignment: CrossAxisAlignment.end,
+      ),
+    ].toRow(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    ),
+  ).paddingDirectional(
+    top: 5,
+  );
+}
