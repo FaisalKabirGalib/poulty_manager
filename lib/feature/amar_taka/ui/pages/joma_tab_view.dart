98c020f2c6dcb92ffc0205f68288ea03a5a83db6
Navid Rahman <navidrahman92@gmail.com>
2023-12-18 12:57:45 +0600

    Joma uttolon tab view added

---
+++ lib/feature/amar_taka/ui/pages/joma_tab_view.dart
@@ -0,0 +1,46 @@
+// joma_tab_view.dart
+
+import 'package:flutter/material.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+import '../../../../config/theme/color.dart';
+import '../../../../core/Layout/extension.dart';
+
+class JomaTabView extends StatelessWidget {
+  const JomaTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: const Color(0XFF00B74A).withOpacity(0.2),
+            ),
+            color: const Color(0XFF00B74A).withOpacity(0.1),
+          ),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Styled.text('বর্তমান ব্যালেন্স').fontSize(16),
+                  Styled.text('৳ ১৫০০.০০')
+                      .textColor(AppColors.successColor)
+                      .fontSize(24)
+                      .fontWeight(FontWeight.bold),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/feature/amar_taka/ui/pages/joma_uttolon_screen.dart
+++ lib/feature/amar_taka/ui/pages/joma_uttolon_screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:poulty_manager/config/constant/constant.dart';
+import 'package:poulty_manager/feature/amar_taka/ui/pages/joma_tab_view.dart';
+import 'package:poulty_manager/feature/amar_taka/ui/pages/uttolon_tab_view.dart';
+import 'package:poulty_manager/feature/shared/components/custom_tabbar.dart';
 import 'package:styled_widget/styled_widget.dart';
 
+import '../../../../config/theme/color.dart';
 import '../../../../core/Layout/extension.dart';
 import '../../../batch/presentation/functions/utils.dart';
 
-class JomaUttolonScreen extends StatelessWidget {
+class JomaUttolonScreen extends StatefulWidget {
   const JomaUttolonScreen({super.key});
+
+  @override
+  State<JomaUttolonScreen> createState() => _JomaUttolonScreenState();
+}
+
+class _JomaUttolonScreenState extends State<JomaUttolonScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +35,20 @@ class JomaUttolonScreen extends StatelessWidget {
         onBack: () {
           Navigator.pop(context);
         },
+      ),
+      SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: CustomTabBar(tabController: tabController, tabs: const [
+          Tab(text: 'জমা'),
+          Tab(text: 'উত্তলন'),
+        ], tabViews: const [
+          // Joma Tab View
+
+          JomaTabView(),
+
+          // Uttolon Tab View
+          UttolonTabView(),
+        ]),
       )
     ].toColumn().parent(page);
   }
