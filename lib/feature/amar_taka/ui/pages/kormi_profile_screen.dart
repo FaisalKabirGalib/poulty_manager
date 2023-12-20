0ee5f7574a2825df32e3ded6f48d528d10be4e4c
Navid Rahman <navidrahman92@gmail.com>
2023-12-20 12:02:10 +0600

    Update kormi_profile_screen.dart

--- lib/feature/amar_taka/ui/pages/kormi_profile_screen.dart
+++ lib/feature/amar_taka/ui/pages/kormi_profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:poulty_manager/feature/batch/presentation/functions/utils.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 import '../../../../config/constant/constant.dart';
@@ -11,81 +10,90 @@ class KormiProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return [
-      SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: [
-          [
-            Styled.icon(Icons.arrow_back).gestures(onTap: () {
-              Navigator.pop(context);
-            }),
-            const Spacer(),
-            Styled.icon(Icons.edit).gestures(onTap: () {}),
-          ].toRow(),
-          KSized.h12,
-          [
-            ClipOval(
-              child: Image.network(
-                'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                width: 100,
-                height: 100,
-                fit: BoxFit.fill,
+    return Stack(
+      children: [
+        [
+          SizedBox(
+            child: [
+              [
+                Styled.icon(Icons.arrow_back).gestures(onTap: () {
+                  Navigator.pop(context);
+                }),
+                const Spacer(),
+                Styled.icon(Icons.edit).gestures(onTap: () {}),
+              ].toRow(),
+              KSized.h12,
+              [
+                ClipOval(
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                KSized.h8,
+                [
+                  Styled.text('মোঃ মাছুদ পারভেজ')
+                      .fontSize(18)
+                      .textColor(Colors.black),
+                  KSized.w4,
+                  Styled.text('(মিন্টু)').fontSize(18).textColor(Colors.grey),
+                ].toRow(mainAxisAlignment: MainAxisAlignment.center),
+                KSized.h12,
+                KSized.h12,
+                contentBox(title: 'কর্মী আইডি', value: '01223344'),
+                KSized.h8,
+                contentBox(title: 'মোবাইল নাম্বার', value: '01700000000'),
+                KSized.h8,
+                contentBox(title: 'ঠিকানা', value: 'বাংলাদেশ, ঢাকা, মিরপুর'),
+              ].toColumn(),
+            ].toColumn(),
+          ),
+        ]
+            .toColumn()
+            .padding(
+              horizontal: 20,
+            )
+            .parent(page),
+
+        // Delete button
+        Positioned(
+          bottom: 10,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width - 40,
+            height: 45,
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: Delete kormi
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: const BorderSide(
+                  color: AppColors.dangerColor,
+                ),
+              ),
+              child: [
+                const Icon(
+                  Icons.delete_forever_outlined,
+                  color: AppColors.dangerColor,
+                ),
+                Styled.text('ডিলিট করুন')
+                    .textColor(AppColors.dangerColor)
+                    .fontSize(16)
+                    .padding(left: 10),
+              ].toRow(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
               ),
             ),
-            KSized.h8,
-            [
-              Styled.text('মোঃ মাছুদ পারভেজ')
-                  .fontSize(18)
-                  .textColor(Colors.black),
-              KSized.w4,
-              Styled.text('(মিন্টু)').fontSize(18).textColor(Colors.grey),
-            ].toRow(mainAxisAlignment: MainAxisAlignment.center),
-            KSized.h12,
-            KSized.h12,
-            contentBox(title: 'কর্মী আইডি', value: '01223344'),
-            KSized.h8,
-            contentBox(title: 'মোবাইল নাম্বার', value: '01700000000'),
-            KSized.h8,
-            contentBox(title: 'ঠিকানা', value: 'বাংলাদেশ, ঢাকা, মিরপুর'),
-          ].toColumn(),
-        ].toColumn(),
-      ),
-
-      // Delete button
-      SizedBox(
-        height: 45,
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            side: const BorderSide(
-              color: AppColors.dangerColor,
-            ),
-          ),
-          child: [
-            const Icon(
-              Icons.delete_forever_outlined,
-              color: AppColors.dangerColor,
-            ),
-            Styled.text('ডিলিট করুন')
-                .textColor(AppColors.dangerColor)
-                .fontSize(16)
-                .padding(left: 10),
-          ].toRow(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-          ),
+          ).padding(horizontal: 20),
         ),
-      ),
-    ]
-        .toColumn()
-        .padding(
-          horizontal: 20,
-        )
-        .parent(page);
+      ],
+    );
   }
 
   SizedBox contentBox({required String title, required String value}) {
